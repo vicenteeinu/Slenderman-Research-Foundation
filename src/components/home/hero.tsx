c54a607093d6c86f4189962df079dc060d46a44f
vicenteeinu <vicente.acg18@gmail.com>
2026-07-23 23:07:48 -0400

    refactor: rename Appearences title to First Appearances in Hero section

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <>
         <section>
-        <h2 className="text-4xl font-bold text-shadow-lg/25">
+        <h2 className="text-4xl font-bold text-shadow-lg/10">
             Who is Slenderman?
         </h2>
 
@@ -29,8 +29,8 @@ export default function Hero() {
 
                 <div className="flex-1">
 
-                    <h2 className="text-4xl font-bold text-shadow-lg/25 py-6">
-                         Appearences
+                    <h2 className="text-4xl font-bold text-shadow-lg/10 py-6">
+                         First Appearances
                     </h2>
 
                     <p className="text-justify text-lg">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
